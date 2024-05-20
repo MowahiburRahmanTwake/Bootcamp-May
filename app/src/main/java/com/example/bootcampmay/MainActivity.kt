9d8cd7325c300310b602e921549c5782c4530625
Md. Mowahibur Rahman Twake <twake.edu@gmail.com>
2024-05-20 22:57:15 +0600

    index ul desigin

---
+++ app/src/main/java/com/example/bootcampmay/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.bootcampmay
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.bootcampmay.ui.HomeFragment
+import com.example.bootcampmay.utils.fragmentAdd
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        fragmentAdd(this,HomeFragment())
+    }
+}
---
+++ app/src/main/java/com/example/bootcampmay/utils/MyUtils.kt
@@ -0,0 +1,22 @@
+package com.example.bootcampmay.utils
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import com.example.bootcampmay.R
+
+fun fragmentAdd(activity: FragmentActivity, fragment: Fragment, bundle: Bundle? = null ) {
+
+    fragment.arguments = bundle
+
+    val sn = "${fragment.javaClass.simpleName}"
+
+    activity?.supportFragmentManager?.beginTransaction()?.apply {
+        addToBackStack(sn)
+        add(R.id.containerView, fragment, sn)
+        commit()
+    }
+
+
+
+}
